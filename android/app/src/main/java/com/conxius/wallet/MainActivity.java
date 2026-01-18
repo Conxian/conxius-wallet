938a864e98444ccafc8d2c388676324d86295868
bo_thebig <botshelomokokoka@gmail.com>
2026-01-18 22:13:09 +0200

    fix: resolve android build issues (gradle 8, manifest export, mainactivity)

--- android/app/src/main/java/com/conxius/wallet/MainActivity.java
+++ android/app/src/main/java/com/conxius/wallet/MainActivity.java
@@ -8,14 +8,5 @@ import com.getcapacitor.Plugin;
 import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    // Initializes the Bridge
-    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-      // Additional plugins you've installed go here
-      // Ex: add(TotallyAwesomePlugin.class);
-    }});
-  }
+  // No need to override onCreate unless adding custom logic
 }
