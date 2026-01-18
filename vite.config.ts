f2b57f5e0a863dcd5e9b18a2f93caaa32d320d3c
bo_thebig <botshelomokokoka@gmail.com>
2026-01-18 20:29:15 +0200

    feat: add wasm support and fix buffer conversion in signer
    Add vite-plugin-wasm and vite-plugin-top-level-await for WebAssembly support
    Fix buffer conversion issues in signer.ts by using Buffer.from()
    Update package dependencies and configuration files accordingly

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -11,6 +13,8 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [
+        wasm(),
+        topLevelAwait(),
         react(),
         nodePolyfills({
           include: ['buffer', 'stream', 'util', 'crypto'],
